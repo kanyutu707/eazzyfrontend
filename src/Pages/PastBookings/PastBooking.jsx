668fd8143e60bae8606cb51cb27f1bd32d56b895
kanyutu707 <kanyutualfred707@gmail.com>
2024-10-22 23:12:17 +0300

    past jobs loading

--- src/Pages/PastBookings/PastBooking.jsx
+++ src/Pages/PastBookings/PastBooking.jsx
@@ -6,12 +6,16 @@ import { MdArrowCircleRight } from "react-icons/md";
 import { IoMdArrowDroprightCircle } from "react-icons/io";
 import house from '../../assets/house.jpg'
 import { useNavigate } from 'react-router-dom';
+import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
 
 const PastBooking = () => {
     const navigate=useNavigate();
+    const [isLoading, setIsLoading]=useState(false);
+    const [errorMessage, setErrorMessage]=useState("");
     const [applications, setApplications] = useState([]);
     useEffect(()=>{
       const fetchData=async ()=>{
+        setIsLoading(true);
         try {
           const response=await fetch("https://eazzybackend-production.up.railway.app/applications/getAll",{
             headers:{
@@ -23,11 +27,13 @@ const PastBooking = () => {
             throw new Error(`Network response was not ok`);
           }
           const data=await response.json();
+          setIsLoading(false);
           console.log(data)
           const loggedIn=parseInt(sessionStorage.getItem('id'))
           const filteredData=data.filter(userapplication=>(userapplication.applicant.id===loggedIn && userapplication.applicationStatus==="INACTIVE"))
           setApplications(filteredData);
         } catch (error) {
+          setErrorMessage("Unable to fetch data");
           throw new Error(`Error encounted, ${error}`)
         }
       };
@@ -35,6 +41,22 @@ const PastBooking = () => {
      
     }, []);
 
+  const pastJobs=(
+    <>
+       {applications.map((application)=>(
+                <tr key={application.id}>
+                        <td>1</td>
+                        <td>{application.posting?.title}</td>
+                        <td>{application.posting?.postType}</td>
+                        <td>{application.posting?.description}</td>
+                        <td>{application.posting?.salary}</td>
+                        <td>{application.applicationDate}</td>
+                        <td></td>
+                        <td> </td>
+                </tr>
+            ))}
+    </>
+  )
   return (
     <div className='pastbookingscontainer'>
     <header>PAST APPLICATION</header>
@@ -60,18 +82,8 @@ const PastBooking = () => {
                 <th></th>
             </thead>
             <tbody>
-            {applications.map((application)=>(
-                <tr key={application.id}>
-                        <td>1</td>
-                        <td>{application.posting?.title}</td>
-                        <td>{application.posting?.postType}</td>
-                        <td>{application.posting?.description}</td>
-                        <td>{application.posting?.salary}</td>
-                        <td>{application.applicationDate}</td>
-                        <td></td>
-                        <td> </td>
-                </tr>
-            ))}
+                {isLoading?<LoadingSpinner/>:pastJobs}
+                {errorMessage&&<div className='error'>errorMessage</div>}
             </tbody>
        
         </table>
