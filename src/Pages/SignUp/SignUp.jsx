1a4c930b753e375918531a0d5062729f37879859
kanyutu707 <kanyutualfred707@gmail.com>
2024-10-22 22:17:46 +0300

    sign up page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import './SignUp.css'
 import { Link, useNavigate } from 'react-router-dom'
+import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
 
 const SignUp = () => {
     const navigate=useNavigate();
+    const [isLoading, setIsLoading]=useState(false);
+    const [errorMessage, setErrorMessage]=useState("");
     const [formData, setFormData]=useState({
         "firstName":"",
         "lastName":"",
@@ -15,6 +18,7 @@ const SignUp = () => {
         setFormData({...formData, [e.target.name]: e.target.value});
     };
     const handleSubmit=async(e)=>{
+        setIsLoading(true);
         e.preventDefault();
         try {
             const response=await fetch("https://eazzybackend-production.up.railway.app/authenticate/register", {
@@ -27,39 +31,49 @@ const SignUp = () => {
             if(!response.ok){
                 throw new Error(`Network response was not ok, ${response}`);
             }
+            setTimeout=(()=>{
+                setIsLoading(false);
+            }, 5000)
             const data=await response.json();
 
             if (data){
                     navigate('../signin')
             }
         } catch (error) {
+            setErrorMessage("Unable to register")
              throw new Error(`There was a problem with your fetch operation, ${error}`);
         }
     }
+    const signup=(
+        <div className='signupform'>
+        <form onSubmit={handleSubmit}>
+            <header>SIGN UP FORM</header>
+            <span className="input_group">
+                <label htmlFor="">FIRST NAME</label>
+                <input type="text" placeholder='FIRST NAME' onChange={handleChange} value={formData.firstName} name='firstName'/>
+            </span>
+            <span className="input_group">
+                <label htmlFor="">LAST NAME</label>
+                <input type="text" placeholder='LAST NAME' onChange={handleChange} value={formData.lastName} name='lastName' />
+            </span>
+            <span className="input_group">
+                <label htmlFor="">EMAIL</label>
+                <input type="email" placeholder='EMAIL' onChange={handleChange} value={formData.email} name='email'/>
+            </span>
+            <span className="input_group">
+                <label htmlFor="">PASSWORD</label>
+                <input type="password" placeholder='PASSWORD' onChange={handleChange} value={formData.password} name='password'/>
+            </span>
+            <button>SIGN UP</button>
+            <span>ALREADY HAVE AN ACCOUNT <Link to='../signin'>SIGN IN</Link></span>
+        </form>
+        </div>
+    )
   return (
-    <div className='signupform'>
-    <form onSubmit={handleSubmit}>
-        <header>SIGN UP FORM</header>
-        <span className="input_group">
-            <label htmlFor="">FIRST NAME</label>
-            <input type="text" placeholder='FIRST NAME' onChange={handleChange} value={formData.firstName} name='firstName'/>
-        </span>
-        <span className="input_group">
-            <label htmlFor="">LAST NAME</label>
-            <input type="text" placeholder='LAST NAME' onChange={handleChange} value={formData.lastName} name='lastName' />
-        </span>
-        <span className="input_group">
-            <label htmlFor="">EMAIL</label>
-            <input type="email" placeholder='EMAIL' onChange={handleChange} value={formData.email} name='email'/>
-        </span>
-        <span className="input_group">
-            <label htmlFor="">PASSWORD</label>
-            <input type="password" placeholder='PASSWORD' onChange={handleChange} value={formData.password} name='password'/>
-        </span>
-        <button>SIGN UP</button>
-        <span>ALREADY HAVE AN ACCOUNT <Link to='../signin'>SIGN IN</Link></span>
-    </form>
-    </div>
+    <>
+        {isLoading?<LoadingSpinner/>:signup}
+        {errorMessage&&<div className='error'>{errorMessage}</div>}
+    </>
   )
 }
 
