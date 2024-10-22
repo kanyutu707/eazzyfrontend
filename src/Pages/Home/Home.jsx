79461de8f887db028308b561d1bfca0a25a07637
kanyutu707 <kanyutualfred707@gmail.com>
2024-10-22 22:58:06 +0300

    Home loading

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,12 @@ import './Home.css'
 import { IoIosArrowDropleftCircle, IoMdArrowDropleft, IoMdArrowDroprightCircle } from "react-icons/io";
 import { MdArrowCircleRight } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
 
 const Home = () => {
   const [jobs, setJobs] = useState([]);
+  const[isLoading, setIsLoading]=useState(false);
+  const[errorMessage, setErrorMessage]=useState("");
  
   const navigate = useNavigate();
 
@@ -15,6 +18,7 @@ const Home = () => {
 
   useEffect(()=>{
     const fetchData=async ()=>{
+      setIsLoading(true);
       try {
         const response=await fetch("https://eazzybackend-production.up.railway.app/jobs/getAll",{
           headers:{
@@ -26,16 +30,35 @@ const Home = () => {
           throw new Error(`Network response was not ok`);
         }
         const data=await response.json();
+        setIsLoading(true);
         const filteredData=data.filter(userapplication=>(userapplication.postingStatus==="ACTIVE"))
         setJobs(filteredData);
       } catch (error) {
+        setErrorMessage("Unable to fetch data");
         throw new Error(`Error encounted, ${error}`)
       }
     };
     fetchData();
   }, []);
    
-
+const jobData=(
+  <>
+    {jobs.map((job) => (
+          <span key={job.id} className='containersection'>
+            <h3 className='header'>{job.title}</h3>
+            <span className='others'><h3>EMPLOYMENT TYPE: </h3> <h4>{job.postType}</h4></span>
+            <span className='others'><h3>SALARY: </h3><h4>{job.salary}</h4></span>
+            <span className="detailsection">
+              <header>DESCRIPTION</header>
+              <h3>{job.description}</h3>
+            </span>
+           
+              <button onClick={() => moveToView(job.id)}>VIEW / APPLY</button>
+           
+          </span>
+        ))}
+  </>
+)
   return (
     <div className='homecontainer'>
       <header>HOME</header>
@@ -70,20 +93,8 @@ const Home = () => {
         <MdArrowCircleRight />
       </section>
       <section className="datasection">
-        {jobs.map((job) => (
-          <span key={job.id} className='containersection'>
-            <h3 className='header'>{job.title}</h3>
-            <span className='others'><h3>EMPLOYMENT TYPE: </h3> <h4>{job.postType}</h4></span>
-            <span className='others'><h3>SALARY: </h3><h4>{job.salary}</h4></span>
-            <span className="detailsection">
-              <header>DESCRIPTION</header>
-              <h3>{job.description}</h3>
-            </span>
-           
-              <button onClick={() => moveToView(job.id)}>VIEW / APPLY</button>
-           
-          </span>
-        ))}
+        {isLoading?<LoadingSpinner/>:jobData}
+        {errorMessage&&<div className='error'>errorMessage</div>}
       </section>
     </div>
   );
