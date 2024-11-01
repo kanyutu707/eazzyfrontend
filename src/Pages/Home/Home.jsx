64fba037d6bec0ef3496085f20af99d98700c7c4
kanyutu707 <kanyutualfred707@gmail.com>
2024-11-01 09:00:50 +0300

    update home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -61,23 +61,20 @@ const jobData=(
   <>
     {searchItem.length>1 ? ( <>
     {filteredResults.map((job) => (
-          <span key={job.id} className='containersection'>
+          <span key={job.id} className='containersection' onClick={() => moveToView(job.id)}>
             <h3 className='header'>{job.title}</h3>
             <span className='others'><h3>EMPLOYMENT TYPE: </h3> <h4>{job.postType}</h4></span>
             <span className='others'><h3>SALARY: </h3><h4>{job.salary}</h4></span>
             <span className="detailsection">
               <header>DESCRIPTION</header>
               <h3>{job.description}</h3>
-            </span>
-           
-              <button onClick={() => moveToView(job.id)}>VIEW / APPLY</button>
-           
+            </span> 
           </span>
         ))}
   </>) : (
      <>
      {jobs.map((job) => (
-           <span key={job.id} className='containersection'>
+           <span key={job.id} className='containersection' onClick={() => moveToView(job.id)}>
              <h3 className='header'>{job.title}</h3>
              <span className='others'><h3>EMPLOYMENT TYPE: </h3> <h4>{job.postType}</h4></span>
              <span className='others'><h3>SALARY: </h3><h4>{job.salary}</h4></span>
@@ -86,7 +83,7 @@ const jobData=(
                <h3>{job.description}</h3>
              </span>
             
-               <button onClick={() => moveToView(job.id)}>VIEW / APPLY</button>
+               
             
            </span>
          ))}
